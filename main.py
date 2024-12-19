4266802d3ad30ac154089a20c572dc395d48bed8
Alex1397Pearce <alx13mikh@gmail.com>
2024-12-19 11:14:28 +0600

    Выполнено задание 1

--- main.py
+++ main.py
@@ -20,11 +20,37 @@ class Bird(Animal):
     def Move(self):
         print("I belive I can flyyy")
 
-some_animal = Dog()
-
-some_animal.Move()
-some_animal.Speak()
+# some_animal = Dog()
+#
+# test = some_animal.Move()
+# print(test)
+# some_animal.Speak()
 
 
 # Задание 2
+
+class Person():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def Speak(self):
+        print(f"My name is {self.name}.")
+
+class Student(Person):
+    def __init__(self, name, age, grade):
+        super().__init__(name, age)
+        self.grade = grade
+
+    def Study(self, thing):
+        print(f"I have grade {self.grade} and I study {thing}")
+
+
+# Bob = Student("Bob", 23, 4)
+#
+# Bob.Speak()
+# Bob.Study("math")
+
+
+# Задание 3
 
